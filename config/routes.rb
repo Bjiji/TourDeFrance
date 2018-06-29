bcf422953f5df553ec0dd02fae7025afa3c33f7e
Hugo Lafayette <hugo.lafayette@temis.com>
2018-06-29 13:51:52 +0200

    fix yj_stage_results

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
   resources :stage_locations
-  resources :races, :cyclists, :ig_race_results, :ig_stage_results, :ite_stage_results,:mountain_stage_results,:race_runners,:race_teams,:stages,:stat_ages,:stat_countries,:stat_miscs,:stat_stage_diffs,:teams,:yj_stage_results
+  resources :races
+  resources :cyclists
+  resources :ig_race_results
+  resources :ig_stage_results
+  resources :ite_stage_results
+  resources :mountain_stage_results
+  resources :race_runners
+  resources :race_teams
+  resources :stages
+  resources :stat_ages
+  resources :stat_countries
+  resources :stat_miscs
+  resources :stat_stage_diffs
+  resources :teams
+  resources :yj_stage_results
   root :to => "races#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
