498834a395e7504eb2a40cc201275eb0f00f13fb
Hugo Lafayette <hugo.lafayette@temis.com>
2018-06-29 13:16:27 +0200

    fix yj_stage_results

--- app/controllers/yj_stage_results_controller.rb
+++ app/controllers/yj_stage_results_controller.rb
@@ -17,6 +17,9 @@ class YjStageResultsController < ApplicationController
 
   # GET /yj_stage_results/1/edit
   def edit
+    def edit
+      @yj_stage_result = YjStageResults.find(params[:id])
+    end
   end
 
   # POST /yj_stage_results
@@ -30,12 +33,17 @@ class YjStageResultsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /yj_stage_results/1
   def update
-    if @yj_stage_result.update(yj_stage_result_params)
-      redirect_to @yj_stage_result, notice: 'Yj stage results was successfully updated.'
-    else
-      render :edit
+    @yj_stage_result = YjStageResults.find(params[:id])
+
+    respond_to do |format|
+      if @yj_stage_result.update_attributes(params.required(:yj_stage_results).permit!)
+        format.html {redirect_to @yj_stage_result, :notice => 'yj stage result was successfully updated.'}
+        format.json {head :no_content}
+      else
+        format.html {render :action => "edit"}
+        format.json {render :json => @yj_stage_result.errors, :status => :unprocessable_entity}
+      end
     end
   end
 
